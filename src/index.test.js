dbf875b721a459aef783f69f7522866bdce06c98
Shakhzod Ikromov <aabbcc.double@gmail.com>
2020-08-19 20:34:25 +0300

    #7 Add `collect` command

--- src/index.test.js
+++ src/index.test.js
@@ -1,31 +1,39 @@
-it("should check for aws configuration", () => {
-    jest.mock("./s3_credentials.js", () => jest.fn(
-        () => ({ isConfigReady: false })
-    ))
+import commander from "commander"
+import swatcherVersion from "../version.js"
+import collectCommand from "./collect_command.js"
 
-    expect(() => require("./index.js")).toThrowError()
+jest.mock("commander", () => {
+    let mock = {}
+    mock.version = jest.fn().mockReturnThis()
+    mock.name = jest.fn().mockReturnThis()
+    mock.description = jest.fn().mockReturnThis()
+    mock.parse = jest.fn()
+
+    mock.command = jest.fn(() => {
+        let mock = jest.fn()
+        mock.command = jest.fn().mockReturnThis()
+        mock.description = jest.fn().mockReturnThis()
+        mock.action = jest.fn().mockReturnThis()
+        return mock
+    })
+
+    return mock
 })
 
-it("should create a new AWS endpoint", () => {
-    jest.mock("aws-sdk", () => ({
-        Endpoint: jest.fn(),
-        S3: jest.fn()
-    }))
+jest.mock("./collect_command.js", () => ({ __esModule: true, default: jest.fn() }))
 
-    jest.mock("./s3_credentials.js", () => jest.fn(
-        () => ({ isConfigReady: true, endpoint: 'aws_endpoint', accessKey: 'aws_key', secretKey: 'secret' })
-    ))
 
-    let aws = require("aws-sdk")
-    let s3cred = require("./s3_credentials.js")
-
+it("", () => {
     require("./index.js")
+    expect(commander.version).toHaveBeenCalledWith(swatcherVersion)
+    expect(commander.name).toHaveBeenCalled()
+    expect(commander.description).toHaveBeenCalledTimes(1)
 
-    expect(s3cred).toHaveBeenCalledTimes(1)
-    expect(aws.Endpoint).toHaveBeenCalledWith("aws_endpoint")
-    expect(aws.S3).toHaveBeenCalledWith({
-        endpoint: aws.Endpoint.mock.instances[0],
-        accessKeyId: "aws_key",
-        secretAccessKey: "secret"
-    })
+    expect(commander.command).toHaveBeenCalledTimes(1)
+    expect(commander.command.mock.calls[0][0]).toMatch("collect")
+    expect(commander.command.mock.results[0].value.description).toHaveBeenCalledTimes(1)
+    expect(commander.command.mock.results[0].value.action).toHaveBeenCalledWith(collectCommand)
+    expect(commander.command.mock.results[0].value.action).toHaveBeenCalledTimes(1)
+
+    expect(commander.parse).toHaveBeenCalledTimes(1)
 })
