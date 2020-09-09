77c47afe704b18d16e8c1d591e866911ced8b3e2
Shakhzod Ikromov <aabbcc.double@gmail.com>
2020-09-09 13:54:41 +0300

    #40 Implement `collect <dir>` command

--- src/__tests__/collect_command.test.js
+++
@@ -1,5 +0,0 @@
-import collectCommand from '../collect_command'
-
-it('should do nothing', async () => {
-  await expect(() => collectCommand('')).rejects.toThrowError()
-})
--- src/collect_command.js
+++ src/collect_command.js
@@ -1,3 +1,31 @@
+import CI from './ci'
+import S3 from './s3'
+import pathLister, { getRealPath } from './path_lister'
+import isPNG from './is_png'
+
 export default async function (dir) {
-  throw new Error('Not implemented yet')
+  const s3 = new S3()
+  const ci = new CI()
+  if (!ci.isVariablesReady) throw new Error('CI variables aren\'t ready yet')
+
+  dir = await getRealPath(dir)
+  const pngFiles = (await listPNGFiles(dir))
+    .map(path => ({ path, key: `${ci.currentBranch}/${ci.buildNumber}/${path.substring(dir.length + 1)}`, contentType: 'image/png' }))
+
+  await uploadFiles(s3, pngFiles)
+}
+
+const uploadFiles = async (s3, items) => {
+  for (const item of items) {
+    console.log(`Uploading '${item.path}' with key '${item.key}' and content-type '${item.contentType}'...`)
+    await s3.upload(item.path, item.key, item.contentType)
+  }
+}
+
+const listPNGFiles = async dir => {
+  const files = await pathLister(dir)
+  const readFiles = await Promise.all(files.map(async path => ({ path, isPNG: await isPNG(path) })))
+  return readFiles
+    .filter(item => item.isPNG)
+    .map(item => item.path)
 }
