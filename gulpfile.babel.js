c7d9ad5f4a710fbe7b9d38b58d38dff38cc505f4
Shakhzod Ikromov <aabbcc.double@gmail.com>
2020-09-12 00:15:10 +0300

    #51 Fix building distribution scripts (#52)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,9 +4,11 @@ import gulp from 'gulp'
 import fs from 'fs'
 
 const makeVersionString = () => {
+  const BUILD_NUMBER_CORRECTION = 18
+
   const MAJOR = '0'
   const MINOR = '0'
-  const PATCH = process.env.GITHUB_RUN_NUMBER ?? '1'
+  const PATCH = (Number(process.env.GITHUB_RUN_NUMBER ?? '1') || 1) + BUILD_NUMBER_CORRECTION
   return `${MAJOR}.${MINOR}.${PATCH}`
 }
 
