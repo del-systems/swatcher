e803df2524c82919e80ae711b4a61d064f6d664f
Shakhzod Ikromov <aabbcc.double@gmail.com>
2021-05-31 23:17:56 +0300

    #139 Ignore text field carets (#140)

---
+++ src/__tests__/compare_pngs_ignore_caret.js
@@ -0,0 +1,25 @@
+import comparePNGs from '../compare_pngs'
+import path from 'path'
+
+const DEFAULT_TIMEOUT = 60000 // in ms
+const fixturePath = name => path.join(__dirname, '../../e2e/fixtures', name)
+
+it('should ignore text field carets on iphone', async () => {
+  const { equal } = await comparePNGs(fixturePath('iphone-caret-1.png'), fixturePath('iphone-caret-2.png'))
+  expect(equal).toBeTruthy()
+}, DEFAULT_TIMEOUT)
+
+it('should ignore text field carets on iphone but nothing else', async () => {
+  const { equal } = await comparePNGs(fixturePath('iphone-caret-3.png'), fixturePath('iphone-caret-2.png'))
+  expect(equal).toBeFalsy()
+}, DEFAULT_TIMEOUT)
+
+it('should ignore text field carets on ipad', async () => {
+  const { equal } = await comparePNGs(fixturePath('ipad-caret-1.png'), fixturePath('ipad-caret-2.png'))
+  expect(equal).toBeTruthy()
+}, DEFAULT_TIMEOUT)
+
+it('should ignore text field carets on ipad but nothing else', async () => {
+  const { equal } = await comparePNGs(fixturePath('ipad-caret-3.png'), fixturePath('ipad-caret-2.png'))
+  expect(equal).toBeFalsy()
+}, DEFAULT_TIMEOUT)
