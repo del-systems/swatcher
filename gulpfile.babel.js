f25b54872c869b0ea07e82731fbc76aa2b3ec0a2
Shakhzod Ikromov <aabbcc.double@gmail.com>
2022-11-27 20:12:43 +0300

    fix gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,11 +2,6 @@ import util from 'util'
 import childProcess from 'child_process'
 import gulp from 'gulp'
 
-const sedCommand = substitute => `sed -i ${process.platform === 'darwin' ? '\'\'' : '' } '${substitute}'`
-
-export const patchStringRequire = () => (
-  util.promisify(childProcess.exec)(`find node_modules -iname '*.js' -type f | xargs grep -l 'string_decoder/' | xargs ${sedCommand('s/string_decoder\\//string_decoder/g')}`)
-)
 export const build = () => util.promisify(childProcess.exec)('npx rollup -c')
 
-export default gulp.series(patchStringRequire, build)
+export default gulp.series(build)
