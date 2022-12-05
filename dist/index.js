433bbf161b1b7788fc9d99f20389e4c339a45263
Shakhzod Ikromov <aabbcc.double@gmail.com>
2022-12-05 16:42:02 +0300

    #173 Update NPM dependencies 2022-12

--- dist/index.js
+++ dist/index.js
@@ -15,7 +15,7 @@ var looksSame = require('looks-same');
 var imageSize = require('image-size');
 var nodeFetch = require('node-fetch');
 
-var swatcherVersion = '1.4.2';
+var swatcherVersion = '1.4.3';
 
 class GithubActionsEnvironment {
   constructor (githubPayload) {
