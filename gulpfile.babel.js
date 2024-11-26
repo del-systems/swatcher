42b707c2857c9c4b27a8e4acb912162e8ac431d9
Shakhzod Ikromov <aabbcc.double@gmail.com>
2024-11-26 12:45:40 +0200

    Remove gulp (#189)
    * Remove gulp
    
    * remove check files gulp

--- gulpfile.babel.js
+++
@@ -1,7 +0,0 @@
-import util from 'util'
-import childProcess from 'child_process'
-import gulp from 'gulp'
-
-export const build = () => util.promisify(childProcess.exec)('npx rollup -c')
-
-export default gulp.series(build)
